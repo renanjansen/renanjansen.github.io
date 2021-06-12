e7f310306387134b5530be0894bbc1ee652788fd
Renan Jansen Lopes de Faria <renanansen@gmail.com>
2021-06-11 23:10:22 -0300

    correções

--- src/sobreMim.js
+++ src/sobreMim.js
@@ -10,7 +10,7 @@ function Sobremim (){
    <img src={perfil} class="border rounded-circle img-fluid border-dark border-5"/>
    <h1 id="sobre-mim" class="pb-3 pt-4 fs-1">Olá, eu sou Renan Jansen</h1>
    <p class="fs-1">Técnico em mecatrônica, estudante de análise e desenvolvimento de sistemas, descobri na pandemia de 2020/2021 um mundo totalmente novo e repleto de oportunidades.</p>
-   <p class="fs-1">Tenho dedicado horas de meus dias em estudos e práticas tendo afiniade e interesse tanto em back quanto front-end.</p>
+   <p class="fs-1">Tenho dedicado horas de meus dias em estudos e práticas, tendo afiniade e interesse tanto em back quanto front-end.</p>
    <p class="fs-1">Busco uma primeira oportunidade no mercado e acredito que a tendo poderei otimizar minhas capacidades e desenvolver outras habilidades latentes.</p>
   </div>
  
